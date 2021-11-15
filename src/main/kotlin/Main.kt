a7f12029f7d7224ac6efe63fb7b1aa0f2acdfedb
Lucas Gabriel Quintana <lucasgabrielquintana@gmail.com>
2021-11-15 10:33:56 -0300

    Menu creado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+// Declaramos los valores de nuestras respuestas
+const val RESPUESTA_AFIRMATIVA = "✅"
+const val RESPUESTA_NEGATIVA = "❌"
+const val RESPUESTA_DUDOSA = "\uD83E\uDD14"
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//Unimos las respuestas con los valores
+val respuestas = mapOf(
+    "Sí" to RESPUESTA_AFIRMATIVA,
+    "Es cierto" to RESPUESTA_AFIRMATIVA,
+    "Totalmente" to RESPUESTA_AFIRMATIVA,
+    "Sin duda" to RESPUESTA_AFIRMATIVA,
+    "Pregunta en otro momento" to RESPUESTA_DUDOSA,
+    "No puedo decirte en este momento" to RESPUESTA_DUDOSA,
+    "Puede que si o puede que no" to RESPUESTA_DUDOSA,
+    "No va a suceder" to RESPUESTA_NEGATIVA,
+    "No cuentes con ello" to RESPUESTA_NEGATIVA,
+    "Definitivamente no" to RESPUESTA_NEGATIVA,
+    "No lo creo" to RESPUESTA_NEGATIVA,
+)
+
+fun main(args: Array<String>) {
+    println("Hola Esto es la Bola 8 Creada en Kotlin. Cual de estas opciones deseas realizar")
+    println("1. Realizar una pregunta")
+    println("2. Revisar todas las respuestas")
+    println("3. Salir")
+
+    val valorIngresado = readLine()
+
+    when(valorIngresado){
+        "1" -> realizarPregunta()
+        "2" -> mostrarRespuestas()
+        "3" -> salir()
+        else -> mostrarError()
+    }
 }
